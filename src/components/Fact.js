9014b92818e3640a4da44c3c188bcd9348147789
Rich T <richard.todd@shopify.com>
2019-08-13 15:42:23 -0400

    Added swag quotes

--- src/components/Fact.js
+++ src/components/Fact.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import tsswag from '../data/tsswag';
 
 class fact extends Component {
   state = {
@@ -6,17 +7,28 @@ class fact extends Component {
   };
 
   getFact = async () => {
-    const fact = await (await fetch(
-      'https://cors-anywhere.herokuapp.com/https://uselessfacts.jsph.pl//random.json?language=en'
-    )).json();
-    var regex = /`/gi;
-    const factText = fact.text.replace(regex, '\'');
-    this.setState(() => ({fact: factText}));
+    const number = Math.random();
+    const threshold = 1;
+    if (number > threshold) {
+      const max = tsswag.length;
+      const min = 1;
+      const random = Math.floor(Math.random() * (max - min + 1) + min);
+      const swag = tsswag[random - 1].text;
+      this.setState(() => ({fact: swag}));
+    } else {
+      // get fact
+      const fact = await (await fetch(
+        'https://cors-anywhere.herokuapp.com/https://uselessfacts.jsph.pl//random.json?language=en'
+      )).json();
+      var regex = /`/gi;
+      const factText = fact.text.replace(regex, "'");
+      this.setState(() => ({fact: factText}));
+    }
   };
 
   componentDidMount() {
     this.getFact();
-    const interval = setInterval(this.getFact, 120000);
+    const interval = setInterval(this.getFact, 12000);
   }
 
   render() {
---
+++ src/data/tsswag.js
@@ -0,0 +1,14 @@
+const tsswag = [
+  {
+    id: 1,
+    text: "There are some juicy IPAs that are basically smoothies",
+    source: "Sheena O",
+  },
+  {
+    id: 2,
+    text: "I'm down and will take my personal hygiene seriously.",
+    source: "Craig R"
+  }
+]
+
+export default tsswag;
