f2d63c03e1da39b59997c8670cc521a8594d2421
Rich T <richard.todd@shopify.com>
2019-09-06 14:45:56 -0400

    Added PSL

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Fact from './components/Fact';
 import Footer from './components/Footer';
 import logo1 from './teapot-1.svg';
-
+import PSL from './components/PSL';
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    psl: false,
+    time: new Date('September 06, 2019 15:30').getTime(),
+  };
+
+
   render() {
+    const backgroundColor = this.state.psl ? {backgroundColor: "#e88131"} : {};
     return (
-      <div className="App">
+      <div className="App" style={backgroundColor}>
         <header className="App-header">
           <img src={logo1} className="App-logo" alt="logo" />
         </header>
         <Fact />
+        {this.state.psl ? (
+          <PSL time={this.state.time} />
+        ) : (
+          ''
+        )}
         <Footer />
       </div>
     );
@@ -21,4 +33,3 @@ class App extends Component {
 }
 
 export default App;
-
