cbdad69f44a2dc449dd7652e1df70b241d330089
Rich T <richard.todd@shopify.com>
2020-03-23 22:55:58 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Quiz from './components/Quiz';
 import Footer from './components/Footer';
 import logo1 from './bucket.png';
@@ -6,16 +7,44 @@ import './styles/App.scss';
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="App-inner">
-        <header className="App-header">
-          <img src={logo1} className="App-logo" alt="logo" />
-        </header>
-        <Quiz />
-        <Footer />
+    <Router>
+      <div className="App">
+        <div className="App-inner">
+          <header className="App-header">
+            <img src={logo1} className="App-logo" alt="logo" />
+            <nav>
+              <h1>Flange & Bucket</h1>
+              <ul>
+                <li>
+                  <Link to="/">Questions</Link>
+                </li>
+                <li>
+                  <Link to="/leaderboard">Leaderboard</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <Switch>
+            <Route path="/leaderboard">
+              <Dave />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </Router>
   );
+};
+
+const Home = () => {
+  return <Quiz />;
+};
+
+const Dave = () => {
+  return <div>Nothing to see here yet...</div>;
 };
 
 export default App;
