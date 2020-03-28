005dc74e75e08e9669c6b15fcd7f993b4b6a1943
Rich T <richard.todd@shopify.com>
2020-03-28 11:07:44 -0400

    Updated form

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -20,25 +20,26 @@ const sortedTeams = sortTeams(teams);
 const Leaderboard = () => {
   return (
     <div className="leaderboard">
-      <span className="leaderboard--header">Team</span>
-      <span className="leaderboard--header">1</span>
-      <span className="leaderboard--header">2</span>
-      <span className="leaderboard--header">3</span>
-      <span className="leaderboard--header">4</span>
-      <span className="leaderboard--header">5</span>
-      <span className="leaderboard--header">6</span>
-      <span className="leaderboard--header">7</span>
-      <span className="leaderboard--header">8</span>
-      <span className="leaderboard--header">9</span>
-      <span className="leaderboard--header">10</span>
-      <span className="leaderboard--header">Total</span>
+      <span className="leaderboard__header--rounds">Rounds</span>
+      <span className="leaderboard__header leaderboard__header--team">Team</span>
+      <span className="leaderboard__header">1</span>
+      <span className="leaderboard__header">2</span>
+      <span className="leaderboard__header">3</span>
+      <span className="leaderboard__header">4</span>
+      <span className="leaderboard__header">5</span>
+      <span className="leaderboard__header">6</span>
+      <span className="leaderboard__header">7</span>
+      <span className="leaderboard__header">8</span>
+      <span className="leaderboard__header">9</span>
+      <span className="leaderboard__header">10</span>
+      <span className="leaderboard__header">Total</span>
       {sortedTeams.map((team, index) => {
         const { scores } = team;
         let init = 0;
         const totalScore = scores.reduce((a, b) => a + b.score, init);
         return (
           <React.Fragment key={index}>
-            <span>
+            <span className="leaderboard__team">
               {team.name}({team.members})
             </span>
             {scores.map((score, index) => (
