3f184a567b05e7bafd21aa4070010d1a81e8c6e2
Rich T <richard.todd@shopify.com>
2020-04-01 16:22:46 -0400

    Answers for round 4

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -35,9 +35,6 @@ const Leaderboard = props => {
       <span className="leaderboard__header">5</span>
       <span className="leaderboard__header">6</span>
       <span className="leaderboard__header">7</span>
-      <span className="leaderboard__header">8</span>
-      <span className="leaderboard__header">9</span>
-      <span className="leaderboard__header">10</span>
       <span className="leaderboard__header">Total</span>
       {sortedTeams.map((team, index) => {
         const { scores } = team;
